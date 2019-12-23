976c0575b35a4dd6f46cd6c8ac58dd24ab1f6d8d
xunijy <412294794@qq.com>
2019-12-23 14:25:57 +0800

    tag标签页、tag列表页

--- application/views/jingxuan.php
+++ application/views/jingxuan.php
@@ -15,66 +15,66 @@
     <div class="module-left">
       <div class="">
         <div class="jingxuan-type">
-          <a href="#" class="items">互联网</a>
-          <a href="#" class="items">美食节</a>
-          <a href="#" class="items">互联网</a>
-          <a href="#" class="items">cpu</a>
-          <a href="#" class="items">abc</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
-          <a href="#" class="items">美食</a>
+          <a href="/page/tag" class="items">互联网</a>
+          <a href="/page/tag" class="items">美食节</a>
+          <a href="/page/tag" class="items">互联网</a>
+          <a href="/page/tag" class="items">cpu</a>
+          <a href="/page/tag" class="items">abc</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
+          <a href="/page/tag" class="items">美食</a>
           <div class="hidden" style="display:none;">
-            <a href="#" class="items">互联网</a>
-            <a href="#" class="items">美食节</a>
-            <a href="#" class="items">互联网</a>
-            <a href="#" class="items">cpu</a>
-            <a href="#" class="items">abc</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
-            <a href="#" class="items">美食</a>
+            <a href="/page/tag" class="items">互联网</a>
+            <a href="/page/tag" class="items">美食节</a>
+            <a href="/page/tag" class="items">互联网</a>
+            <a href="/page/tag" class="items">cpu</a>
+            <a href="/page/tag" class="items">abc</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
+            <a href="/page/tag" class="items">美食</a>
           </div>
           <p class="ta-c pt10 pb5"><a href="javascript:;" class="btn" id="btn_more">展开更多 +</i>
           </a></p>
@@ -87,7 +87,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
@@ -96,7 +96,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
@@ -105,7 +105,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
@@ -114,7 +114,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
@@ -123,7 +123,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
@@ -132,7 +132,7 @@
               <div class="desc">
                 <a href="" target="_blank" class="title">柿子和什么不能一起吃？</a>
                 <div class="summary">猫咪除了在猫砂盆尿尿外，还有可能会在各种地方解决内急问题，比如墙角、柜子里、床底、甚至是你的被窝。一旦垫子、被子上沾染了猫尿的气味，就很难处理。虽然被罩，垫子套等可以清洗，可是渗透到棉花上的猫尿味却很难去除，直接把这床被子给扔了吧太可惜，可是又实在找不到好的去味方法，难道猫尿味就真的这么难去除吗？并不是呦。</div>
-                <p class="after-cls"><span class="tag"><a href="#">柿子</a><a href="#">不能</a><a href="#">一起吃</a><a href="#">什么</a></span>
+                <p class="after-cls"><span class="tag"><a href="/page/tag">柿子</a><a href="/page/tag">不能</a><a href="/page/tag">一起吃</a><a href="#">什么</a></span>
                 <span class="num">0个回答</span>
                 <span class="time">2020-2-22</span></p>
               </div>
