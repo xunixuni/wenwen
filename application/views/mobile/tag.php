065d6eec791b24e1a0c03c4dbbbaaccf13e7af6a
xunijy <412294794@qq.com>
2020-01-06 15:47:46 +0800

    h5 网站地图、标签详情页

---
+++ application/views/mobile/tag.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <?php include_once('templete/pub_head.php') ?>
+    </head>
+    <body>
+    <?php include_once('templete/top_head.php') ?>
+    <div class="container pt44 pb50">
+      <div class="header">
+        <div class="header-title">
+          <a href="javascript:history.go(-1);" id="back">
+            <i class="ico-header-back"></i>
+          </a>
+          <h4>标签：美食</h4>
+          <a href="javascript:;" class="open-popup" data-target="#sousuo">
+              <i class="ico-header-search"></i>
+          </a>
+        </div>
+      </div>
+      <div class="jinxuan-list biaoqian">
+        <div class="title">关于<em class="col-default">美食</em>标签的热门问题</div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃柿子和不能一起吃柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+        <div class="jinxuan-item">
+          <div class="desc">
+            <a href="/mobile/pages" class="tit">柿子和什么不能一起吃柿子和不能一起吃柿子和什么不能一起吃？</a>
+          </div>
+          <div class="tag">
+            <a href="/mobile/tag">柿子</a>
+            <a href="/mobile/tag">不能</a>
+            <a href="/mobile/tag">一起吃</a>
+            <a href="/mobile/tag">什么</a>
+          </div>
+        </div>
+      </div>
+      <div class="about-link">
+        <a href="/mobile/about-wenwen">关于我们</a>
+        <a href="/mobile/about-wenwen">隐私声明</a>
+        <a href="/mobile/about-wenwen" >联系我们</a>
+        <a href="/mobile/sitemap">网站地图</a>
+        <a href="/mobile/about-wenwen">帮助中心</a>
+        <div class="copyright">©2019-2020&nbsp;&nbsp;问问网&nbsp;&nbsp;浙ICP备号&nbsp;&nbsp;<br />业务经营许可证：浙&nbsp;&nbsp;浙公网安备号</div>
+      </div>
+    </div>
+    <?php include_once('templete/pub_foot.php') ?>
+    <?php include_once('templete/tabbar.php') ?>
+    <script type="text/javascript">
+    $(function(){
+
+    })
+    </script>
+    </body>
+</html>
